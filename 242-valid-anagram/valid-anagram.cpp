577a7e7719531d6274d86bdf1122b84758695c17
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-07 11:44:32 +0530

    Time: 46 ms (7.13%) | Memory: 20.9 MB (11.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int size = s.size();
-        if(size!=t.size()){
+        if(s.size()!=t.size()){
             return false;
         }
-        std::unordered_multiset<char> setS(s.begin(), s.end());
-        std::unordered_multiset<char> setT(t.begin(), t.end());
+        unordered_multiset<char> setS(s.begin(), s.end());
+        unordered_multiset<char> setT(t.begin(), t.end());
         return setS == setT;
     }
 };
