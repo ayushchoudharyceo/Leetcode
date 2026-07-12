381d44c5072ef544f68dad3fc7095b1b81dcce32
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-12 16:35:17 +0530

    Time: 36 ms (72.49%) | Memory: 43.8 MB (71.43%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+        if(arr.size()==0) return{};
         vector<int>s_arr= arr;
         sort(s_arr.begin(),s_arr.end());
         unordered_map<int, int> rank;
-        int crr=1;
+        int crr=1, i=0;
         for(int num : s_arr)
         {
             if(rank.find(num) == rank.end())
@@ -14,9 +15,10 @@ public:
             }
         }
         vector<int>result(arr.size());
-        for(int i =0 ;i<arr.size();i++)
+        while(i<arr.size())
         {
             result[i] = rank[arr[i]];
+            i++;
         }
         return result;
     }
