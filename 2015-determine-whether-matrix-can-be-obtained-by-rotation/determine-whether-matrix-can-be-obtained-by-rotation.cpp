aaa6aa7e3dd1c99aa0865991c973e2ec76b7b4e4
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-14 12:18:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (96.18%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    bool findRotation(vector<vector<int>>& matrix, vector<vector<int>>& target) {
+    bool findRotation(vector<vector<int>>& matrix, vector<vector<int>>& tar) {
+        vector<bool> res(4, true);
         int n = matrix.size();
-        for(int k=0;k<4;k++){
+        
         for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+            for(int j = 0; j < n; j++) 
+            {                 
+                if(matrix[i][j] != tar[j][n - 1 - i]) res[0] = false;
+                if(matrix[i][j] != tar[n - 1 - i][n - 1 - j]) res[1] = false;
+                if(matrix[i][j] != tar[n - 1 - j][i]) res[2] = false;
+                if(matrix[i][j] != tar[i][j]) res[3] = false; 
             }
         }
-        for(int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-        if (matrix==target) return true;
-        }
-        return false;
+        
+        return res[0] || res[1] || res[2] || res[3];
     }
 };
