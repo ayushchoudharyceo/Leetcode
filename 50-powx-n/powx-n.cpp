605de40850572ecf26ed690c9f9c87710c32f608
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-16 12:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.60%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);      
+        return pow(x, (long long)n);
+    }
+private:
+    double pow(double x, long long n) {
+        if (n == 0) return 1.0;
+        if (n < 0) return 1.0 / pow(x, -n);
+        if (n % 2 == 1) {
+            return x * pow(x * x, n / 2);
+        }
+        return pow(x * x, n / 2);
     }
 };
