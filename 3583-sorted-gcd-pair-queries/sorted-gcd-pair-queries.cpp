bf48f86a6b8024eef70b07e05e59a1427d5aa5bf
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-18 02:33:10 +0530

    Time: 40 ms (68.67%) | Memory: 127.5 MB (63.86%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -5,28 +5,19 @@ public:
         for (int x : nums) {
             max_val = max(max_val, x);
         }
-        
-        // cnt[i] stores the frequency of number i in nums
         vector<int> cnt(max_val + 1, 0);
         for (int x : nums) {
             cnt[x]++;
         }
-        
-        // G[i] will store the exact number of pairs with GCD exactly equal to i
         vector<long long> G(max_val + 1, 0);
-        
-        // Iterate backwards to compute the exact GCD frequencies using inclusion-exclusion
         for (int i = max_val; i >= 1; --i) {
             long long c = 0;
-            // Count how many elements in nums are multiples of i
             for (int j = i; j <= max_val; j += i) {
                 c += cnt[j];
             }
             
-            // Total pairs where both elements are multiples of i
             long long pairs = c * (c - 1) / 2;
             
-            // Subtract pairs where the GCD is a strict multiple of i (2i, 3i, etc.)
             for (int j = 2 * i; j <= max_val; j += i) {
                 pairs -= G[j];
             }
@@ -34,16 +25,13 @@ public:
             G[i] = pairs;
         }
         
-        // Compute prefix sums of the GCD frequencies
         vector<long long> pref(max_val + 1, 0);
         for (int i = 1; i <= max_val; ++i) {
             pref[i] = pref[i - 1] + G[i];
         }
         
-        // Answer each query using binary search
         vector<int> ans(queries.size());
         for (int i = 0; i < queries.size(); ++i) {
-            // upper_bound finds the first index where the prefix sum is strictly greater than the query
             ans[i] = upper_bound(pref.begin(), pref.end(), queries[i]) - pref.begin();
         }
         
