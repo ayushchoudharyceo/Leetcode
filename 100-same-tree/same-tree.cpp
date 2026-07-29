dfc51f3ce3313af659d1638581cdf46cce224012
Ayush Choudhary <ayushchoudhary.sms@gmail.com>
2026-07-29 12:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (13.43%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    void Traverse(TreeNode* root, vector<int>& result) 
-    { 
-        if (root == nullptr) 
-        {
-            result.push_back(INT_MIN);
-            return;
-        }
-        result.push_back(root->val);
-        Traverse(root->left, result);
-        Traverse(root->right, result);
-    }
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> result1;
-        vector<int> result2;
-
-        Traverse(p, result1);
-        Traverse(q, result2);
-
-        return result1 == result2; 
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
